577d7d5787332736f534b7ddc33a96ad09024853
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-02-16 12:08:20 -0300

    ApiTeste Versao 1.2 Login Controller token completed

--- src/app/Controllers/LoginController.js
+++ src/app/Controllers/LoginController.js
@@ -1,7 +1,40 @@
+const User = require('../Models/User');
+const bcrypt = require('bryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/auth');
+
 class LoginController {
 
-    index(req, res){
+    async index(req, res){
+        const { email, password } = req.body;
 
+        const userExist = await User.findOne({ email })
+
+        if (userExist){
+            return res.status(400).json({
+                error: true,
+                message: "Usuário não existe!"
+            })
+        }
+
+        if (!(await bcrypt.compare(userExist.password, userExist))){
+            return res.status(400).json({
+                error: true,
+                message: "A senha está inválida!"
+            })
+        }
+
+        return res.status(200).json({
+            user:{
+                name: userExist.name,
+                email: userExist.email
+            },
+            token: jwt.sign(
+                {id: userExist._id},
+                config.secret,
+                {expiresIn: config.expireIn}
+                )
+        })
     }
 }
 
