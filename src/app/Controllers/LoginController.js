eaa5c9179a852b44c32f7edf0d0e5793eac3d453
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-02-16 12:15:31 -0300

    ApiTeste Versao 1.21 fully functional route token LoginController

--- src/app/Controllers/LoginController.js
+++ src/app/Controllers/LoginController.js
@@ -1,5 +1,5 @@
 const User = require('../Models/User');
-const bcrypt = require('bryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/auth');
 
@@ -11,20 +11,20 @@ class LoginController {
         const userExist = await User.findOne({ email })
 
         if (userExist){
-            return res.status(400).json({
+            return res.status(400).json({ //validação de se o usuário existe
                 error: true,
                 message: "Usuário não existe!"
             })
         }
 
-        if (!(await bcrypt.compare(userExist.password, userExist))){
+        if (!(await bcrypt.compare(userExist.password, userExist))){ //validação se a senha está errada
             return res.status(400).json({
                 error: true,
                 message: "A senha está inválida!"
             })
         }
 
-        return res.status(200).json({
+        return res.status(200).json({ //retorna os dados do usuário junto a uma decodificação do token pelo id
             user:{
                 name: userExist.name,
                 email: userExist.email
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const UserController = require('./app/Controllers/UserController');
+const LoginController = require('./app/Controllers/LoginController');
 
 const routes = new Router();
 
@@ -8,6 +9,8 @@ const routes = new Router();
 routes.post("/user", UserController.store);
 routes.get("/user", UserController.show);
 routes.get("/userr", UserController.showr);
+
+routes.post("/login", LoginController.index);
 
 
 module.exports = routes;
